7cfc30287bcae342181cad4778708415d2c4771b
dima <97853925+FreazeMood@users.noreply.github.com>
2022-06-17 17:24:00 +0300

    Update shop_table.py

--- shop_table.py
+++ shop_table.py
@@ -2,10 +2,9 @@ from database import MySQLDatabase
 
 import mysql.connector.errors
 
-DBCONFIG = {'host': 'eu-cdbr-west-02.cleardb.net',
-            'user': 'b8a14ff118d075',
-            'password': '0922b736',
-            'database': 'heroku_b7354dfae7a5454'}
+import os
+
+DBCONFIG = os.environ.get("DBCONFIG")
 
 
 class Stock:
